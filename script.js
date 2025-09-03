746b35c33bd06804ef0919be59e15f9228e9ff04
Vishal Wani <93905384+Vishalwani123@users.noreply.github.com>
2025-09-03 16:07:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+const buttons = document.querySelectorAll('.btn');
+
+let currentAudio = null;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const soundName = button.innerText;
+
+        if (soundName === 'stop') {
+            if (currentAudio) {
+                currentAudio.pause();
+                currentAudio.currentTime = 0;
+                currentAudio = null;
+            }
+        } else {
+            if (currentAudio) {
+                currentAudio.pause();
+                currentAudio.currentTime = 0;
+            }
+            currentAudio = new Audio(`sounds/${soundName}.mp3`);
+            currentAudio.play();
+        }
+    });
+});
